6c19dcc3565c6324c2df1767f3a3ba736d41fffc
Laptev Roman <romashlapt@mail.ru>
2025-07-10 15:31:12 +0700

    fix gpt api

--- app/gpt_api.py
+++ app/gpt_api.py
@@ -2,27 +2,21 @@ import os
 from openai import OpenAI, OpenAIError
 
 
-API_KEY = os.getenv("API_GPT_KEY")
-if not API_KEY:
-    raise RuntimeError("OPENAI_API_KEY is not set")
-
-client = OpenAI(api_key=API_KEY, base_url=os.getenv("GPT_URL"))
-
-
 async def category_definition(text: str) -> str:
+    API_KEY = os.getenv("API_GPT_KEY")
+    if not API_KEY:
+        raise RuntimeError("OPENAI_API_KEY is not set")
+
+    client = OpenAI(api_key=API_KEY, base_url=os.getenv("GPT_URL"))
     sys_prompt = (
-        "Ты — специалист по заявкам от клиентов.\n"
-        "Определи категорию жалобы.\n"
-        "Варианты: техническая, оплата, другое.\n"
-        "Ответь одним словом, без дополнительных пояснений."
+        f"Определи категорию жалобы: {text}. Варианты: техническая, оплата, другое."
+        f" Ответ только одним словом. "
     )
-    user_msg = f'Жалоба: "{text}"'
     try:
         response = client.chat.completions.create(
             model="gpt-4o",
             messages=[
                 {"role": "system", "content": sys_prompt},
-                {"role": "user", "content": user_msg},
             ],
             temperature=0.0,
         )
